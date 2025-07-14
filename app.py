0ffbb6aef567036d893a11e6a6ea832921d90e81
talenpool <sulthancgeit@gmail.com>
2025-07-14 19:20:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if layout_type == "Office":
     #     base_image = Image.new("RGB", (1000, 600), color="gray")
     #     st.warning("Could not load floorplan image - assets/office/floorplan.png. Using placeholder.")
 
-    base_image = Image.open("assets/office/test888.png")
+    base_image = Image.open("assets/office/test123.png")
     # asset_paths = {
     #     "Desk": "assets/office/desk.png",
     #     "Computer": "assets/office/computer.png",
